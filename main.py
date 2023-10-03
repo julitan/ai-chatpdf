73f889b937e85f0d6968b016fd60274568b51df6
julitan <49323514+julitan@users.noreply.github.com>
2023-10-03 21:46:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # from dotenv import load_dotenv
 # load_dotenv()
+from typing import Any, Optional, Union
+from uuid import UUID
 from langchain.document_loaders import PyPDFLoader
+from langchain.schema.output import ChatGenerationChunk, GenerationChunk
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -15,6 +18,7 @@ import streamlit as st
 import tempfile
 import os
 from streamlit_extras.buy_me_a_coffee import button
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 button(username="harkjael", floating=True, width=221)
 
@@ -61,11 +65,25 @@ if uploaded_file is not None:
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
 
+    # Stream 받아 줄 Handler 만들기
+    from langchain.callbacks.base import BaseCallbackHandler
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container, initial_text=""):
+            self.container = container
+            self.text=initial_text
+        def on_llm_new_token(self, token: str, **kwargs) -> None:
+            self.text += token
+            self.container.markdown(self.text)
+            
     # Question
     st.header("myChatPDF에게 질문!")
     question = st.text_input("질문을 입력하세요")
     if st.button("질문하기"):
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key)
-        qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-        result = qa_chain({"query": question})
-        st.write(result["result"])
+        with st.spinner('wait for it...'):
+            chat_box = st.empty()
+            stream_handler = StreamHandler(chat_box)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key, streaming=True, callbacks=[stream_handler])
+            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+            qa_chain({"query": question})
+            # result = qa_chain({"query": question})
+            # st.write(result["result"])
